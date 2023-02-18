c8847cc3c63059f3eede29009e2528dbdb1d111b
Abdul Rafay <99marafay@gmail.com>
2023-02-18 05:30:51 +0500

    Adding the Structure of the website

---
+++ API/api.py
@@ -0,0 +1,14 @@
+import requests
+
+API_URL = "https://api-inference.huggingface.co/models/Hello-SimpleAI/chatgpt-detector-roberta"
+headers = {"Authorization": "Bearer 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"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+	
+output = query({
+	"inputs": "I like you. I love you",
+})
+
+print(output)
