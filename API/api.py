a9bb67a3d4fcb542468bcd6932a867a1e6fc0ba9
Abdul Rafay <99marafay@gmail.com>
2023-02-18 05:46:58 +0500

    testing API

--- API/api.py
+++ API/api.py
@@ -6,7 +6,9 @@ headers = {"Authorization": "Bearer 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"}
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
 	return response.json()
-	
+
+
+
 output = query({
 	"inputs": "I like you. I love you",
 })
